dfefc30b84bb9ad592684d5c28e87afffead31f6
Andrew Kind <andrew.w.kind@gmail.com>
2020-08-14 11:17:35 -0600

    Add required to input.

--- js/react-scripts.js
+++ js/react-scripts.js
@@ -5,11 +5,26 @@ const SearchForm = props =>
 {
   // Manage "search" term in state.
   const [search, setSearch] = React.useState( '' );
+  const [results, setResults] = React.useState( [] );
 
   // Handle form submission.
   const submitSearch = event => {
     event.preventDefault();
     // console.log( "Attempted to submit!" ); // Tested and working!
+    // Make a request to the expected URL (API endpoint.)
+    fetch ( `includes/ajax-api.php?search=${search}` )
+      // Convert the response from a JSON string to a JavaScript object / array. 
+      .then( response => response.json() )
+      // Deal with the data.
+      .then( data => {
+        // console.log( data ); // Looks like we're getting results!
+        // Capture our results in the component state.
+        setResults( data );
+      } )
+      // If an error is encountered, notify us (check the browser console.)
+      .catch( error => {
+        throw error;
+      } );
   }
 
   // Return the JSX (render the component.)
@@ -20,6 +35,7 @@ const SearchForm = props =>
         <label htmlFor="search">
           Search People:
           <input
+            required
             type="search"
             name="search"
             id="search"
@@ -30,7 +46,13 @@ const SearchForm = props =>
         <input type="submit" value="Submit Search" />
       </form>
       <h3>Search Results</h3>
-      <ul id="results"></ul>
+      <ul id="results">
+        {results.map( person => (
+          <li key={person.name}>
+            Hi! I am {person.name}. I am {person.age} years old!
+          </li>
+        ) )}
+      </ul>
     </section>
   );
 };
