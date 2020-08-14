3f78f8b16dcf244854a290d01c8393347a9c0d17
Andrew Kind <andrew.w.kind@gmail.com>
2020-08-14 10:52:28 -0600

    Add state for search input.

--- js/react-scripts.js
+++ js/react-scripts.js
@@ -1,20 +1,31 @@
 const reactRoot = document.getElementById( 'root' );
 
 // Search form component.
-const SearchForm = props => {
-  
+const SearchForm = props =>
+{
+  // Manage "search" term in state.
+  const [search, setSearch] = React.useState( '' );
+
+  // Handle form submission.
+  const submitSearch = event => {
+    event.preventDefault();
+    // console.log( "Attempted to submit!" ); // Tested and working!
+  }
+
   // Return the JSX (render the component.)
   return (
     <section>
       <h2>Search Form</h2>
-      <form>
+      <form onSubmit={submitSearch}>
         <label htmlFor="search">
           Search People:
           <input
             type="search"
             name="search"
             id="search"
-            placeholder="Enter term(s)..." />
+            placeholder="Enter term(s)..."
+            value={search}
+            onChange={event => { setSearch( event.target.value ) }} />
         </label>
         <input type="submit" value="Submit Search" />
       </form>
